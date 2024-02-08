33a883c14534305c3fde7a5b9786845204c7e752
Shivendra Saurav <shivendrasaurav@gmail.com>
2024-02-08 15:01:44 +0530

    added docs for views, switched to light mode temporarily

---
+++ docs/src/app/content/views/page.tsx
@@ -0,0 +1,29 @@
+import "../../globals.css";
+import "../../source/main.css";
+import DocsSideNav from "../../templates/docsSideNav";
+import DocsTopNav from "../../templates/docsTopNav";
+
+const Views = () => {
+
+    return(
+        <div className="pageView">
+            <div className="gridRow rowHi-1 rowCenter">
+                <DocsTopNav />
+            </div>
+            <div className="gridRow rowHi-8 rowCenter">
+                <div className="colWd-3">
+                    <DocsSideNav />
+                </div>
+                <div className="colWd-10">
+                    <div className="withPrimaryBackground contentWrapper paddingSmall">
+                        <h1>Views</h1>
+                        <h2>The basic building blocks</h2>
+                    </div>
+                </div>      
+            </div>
+        </div>
+    );
+
+}
+
+export default Views;
